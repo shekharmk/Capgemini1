6d7daea00c0751e849d2558e4a61400118cd49e9
Shekhar Kankale <shekharkankale@gmail.com>
2020-01-30 16:54:17 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package first;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		 
+		System.out.println("Hello World");
+		System.out.print("Welcome to");
+		System.out.printf("%s","Java");
+	}
+
+}
